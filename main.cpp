27a4e519a5959c96be8bd2b92e8598f4c2c18491
ajp109 <andrew.pomfret@york.ac.uk>
2023-01-10 16:18:44 +0000

    Initial commit of project-specific files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include "mbed.h"
 
 int main() {
+
+    int x = 6;
+    int array[] = {0,1,2,3,4,5,6,7,8,9};
+
+    printf("x is located at address %p\n", &x);
+    printf("array is located at address %p\n", &array);
+
     while (true) {
         sleep();
     }
